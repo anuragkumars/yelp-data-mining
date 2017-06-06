c9026193d46425752d14c8e591ff24560c29fc33
anuragkumars <anurag_kumar@usa.com>
2017-06-05 22:24:17 -0700

    Create run_command_windows.bat

---
+++ run_command_windows.bat
@@ -0,0 +1,40 @@
+java -cp bin;^
+lib/cglib-nodep-3.2.4.jar;^
+lib/commons-codec-1.10.jar;^
+lib/commons-exec-1.3.jar;^
+lib/commons-io-2.5.jar;^
+lib/commons-lang3-3.5.jar;^
+lib/commons-logging-1.2.jar;^
+lib/cssparser-0.9.21.jar;^
+lib/gson-2.8.0.jar;^
+lib/guava-21.0.jar;^
+lib/hamcrest-core-1.3.jar;^
+lib/hamcrest-library-1.3.jar;^
+lib/htmlunit-2.24.jar;^
+lib/htmlunit-core-js-2.23.jar;^
+lib/htmlunit-driver-2.24.jar;^
+lib/httpclient-4.5.2.jar;^
+lib/httpcore-4.4.4.jar;^
+lib/httpmime-4.5.2.jar;^
+lib/javax.servlet-api-3.1.0.jar;^
+lib/jetty-io-9.4.1.v20170120.jar;^
+lib/jetty-util-9.4.1.v20170120.jar;^
+lib/jna-4.1.0.jar;^
+lib/jna-platform-4.1.0.jar;^
+lib/junit-4.12.jar;^
+lib/neko-htmlunit-2.24.jar;^
+lib/phantomjsdriver-1.4.0.jar;^
+lib/sac-1.3.jar;^
+lib/serializer-2.7.2.jar;^
+lib/websocket-api-9.2.20.v20161216.jar;^
+lib/websocket-client-9.2.20.v20161216.jar;^
+lib/websocket-common-9.2.20.v20161216.jar;^
+lib/xalan-2.7.2.jar;^
+lib/xercesImpl-2.11.0.jar;^
+lib/xml-apis-1.4.01.jar;^
+lib/client-combined-3.3.1-nodeps.jar ;^
+-DconfigFile=C:/dev/docs/BrightPlan/BrightPlan_B/testing/config/BPlan_RunInfo.properties ;^
+-DUseInputFilePath=C:/dev/docs/BrightPlan/BrightPlan_B/testing/input/ ;^
+-DUseOutputFilePath=C:/dev/docs/BrightPlan/BrightPlan_B/testing/output/ ;^
+-DUseErrorFilePath=C:/dev/docs/BrightPlan/BrightPlan_B/testing/output/ ;^
+org.junit.runner.JUnitCore com.brightplan.automation.projects.yelp.YELP001_DisplaySearchDetails_JUnit > run3.txt
